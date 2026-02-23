678d328fac9897d99fe01a84bf81ae5434482464
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-02-23 23:33:54 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react'
 export const authDataContext = createContext() // context api create = create provider consumer 
 
 const AuthContext = ({children}) => {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://varuno-bbjw.onrender.com"
      let value = {
         serverUrl
      }
