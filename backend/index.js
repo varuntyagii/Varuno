e281812894975f5ed96c5b60f6140795c103e30b
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-02-25 00:12:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -69,7 +69,7 @@ app.use(cookieParser());
 // CORS
 app.use(
   cors({
-    origin: ["https://varuno-1.onrender.com", "https://admin-y1lw.onrender.com"],
+    origin: ["https://varuno-1.onrender.com", "https://admin-y1lw.onrender.com", "varuno.vercel.app"],
     // origin:true,
     credentials: true,
   })
