41d4df7e9b7a059c9d1092c90596900e9884c938
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-02-26 01:50:34 +0530

    Update newsletterSchema.js

--- backend/model/newsletterSchema.js
+++ backend/model/newsletterSchema.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const newsletterSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      trim: true,
+    },
     email: {
       type: String,
       required: true,
